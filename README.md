67bc53a552ee2e531999c706cf0069c9808ffda4
xianx <xianx@users.noreply.github.com>
2023-08-15 12:31:08 +0600

    2 files modified, 1 deleted

--- README.md
+++
@@ -1,3 +0,0 @@
-# stackbliz-astro-starter
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-m59def)
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,20 @@
-function Card() {
+// Card Component
+
+function Card(props) {
   return (
-    <div class="container m-6">
+    <div className="container m-6">
       <a
         href="#"
-        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-3xl shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        className="block max-w-sm p-6 bg-white border border-gray-200 rounded-3xl shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
       >
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Astro Project with Tailwind and React
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {props.title}
         </h5>
-        <p class="font-normal text-gray-700 dark:text-gray-400">
-          This a starter Astro Project with tailwind and{' '}
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          {props.children}
+        </p>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          {props.footer}
         </p>
       </a>
     </div>
