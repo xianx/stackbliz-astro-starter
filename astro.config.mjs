0ffcfca216e1896ff923855726b97c68959c59aa
xianx <xianx@users.noreply.github.com>
2023-08-23 21:34:55 +0600

    3 files modified, 22 added, 19 deleted

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+
 export default defineConfig({
   integrations: [react(), tailwind()]
 });
